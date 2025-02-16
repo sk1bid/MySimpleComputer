1d28e7827eae3ab42c15da8bec2d027fed3fa3ff
is342s12 <is342s12>
2025-02-16 08:16:42 +0700

    Add .h files

---
+++ include/mySimpleComputer.h
@@ -0,0 +1,13 @@
+#ifndef INCLUDE_MY_SIMPLE_COMPUTER_H
+#define INCLUDE_MY_SIMPLE_COMPUTER_H
+
+#include "../mySimpleComputer/sc_memory.h"
+#include "../mySimpleComputer/sc_reg.h"
+#include "../mySimpleComputer/sc_command.h"
+#include "../console/io.h"
+
+extern int memory[MEMORY_SIZE];         // Оперативная память
+extern int accumulator;               // Аккумулятор
+extern int regFlags;                  // Регистр флагов
+extern int instructionCounter;         // Счетчик команд
+#endif
