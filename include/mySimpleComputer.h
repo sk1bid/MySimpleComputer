0e3876e0bbd17832b06ff9b46131443e5065e78c
root <root@artem.>
2025-05-02 14:39:40 +0700

    Add new includes + variables

--- include/mySimpleComputer.h
+++ include/mySimpleComputer.h
@@ -1,6 +1,9 @@
 #ifndef INCLUDE_MY_SIMPLE_COMPUTER_H
 #define INCLUDE_MY_SIMPLE_COMPUTER_H
 
+#include "../console/console_log.h"
+#include "../myBigChars/myBigChars.h"
+#include "../mySimpleComputer/sc_cache.h"
 #include "../mySimpleComputer/sc_command.h"
 #include "../mySimpleComputer/sc_memory.h"
 #include "../mySimpleComputer/sc_reg.h"
@@ -11,6 +14,7 @@ extern int memory[MEMORY_SIZE]; // Оперативная память
 extern int accumulator;         // Аккумулятор
 extern int regFlags;            // Регистр флагов
 extern int instructionCounter;  // Счетчик команд
-extern int is_reading;
-extern int read_address;
+extern int is_reading;          // чтение?
+extern int read_address; // адрес ячейки для считанного значения
+extern int current_cache_line;
 #endif
