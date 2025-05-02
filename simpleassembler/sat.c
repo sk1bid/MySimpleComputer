9af6ef3050b52ded0d960cbd4cc2c6bbb09f6914
root <root@artem.>
2025-05-02 14:28:24 +0700

    Remove some commands

--- simpleassembler/sat.c
+++ simpleassembler/sat.c
@@ -4,45 +4,61 @@
 
 #define MEMORY_SIZE 128
 
-// Define opcodes for all commands
-#define CMD_NOP     0x00
+#define CMD_NOP 0x00
 #define CMD_CPUINFO 0x01
-#define CMD_READ    0x0A
-#define CMD_WRITE   0x0B
-#define CMD_LOAD    0x14
-#define CMD_STORE   0x15
-#define CMD_ADD     0x1E
-#define CMD_SUB     0x1F
-#define CMD_DIVIDE  0x20
-#define CMD_MUL     0x21
-#define CMD_AND     0x34
-#define CMD_MUL2    0x50
-#define CMD_DIV2    0x51
-#define CMD_JUMP    0x28
-#define CMD_JNEG    0x29
-#define CMD_HALT    0x2B
-#define CMD_NOT     0x33
+#define CMD_READ 0x0A
+#define CMD_WRITE 0x0B
+#define CMD_LOAD 0x14
+#define CMD_STORE 0x15
+#define CMD_ADD 0x1E
+#define CMD_SUB 0x1F
+#define CMD_DIVIDE 0x20
+#define CMD_MUL 0x21
+#define CMD_AND 0x34
+#define CMD_JUMP 0x28
+#define CMD_JNEG 0x29
+#define CMD_HALT 0x2B
+#define CMD_NOT 0x33
+#define CMD_JP 0x3A
+#define CMD_JNP 0x3B
 
 int encode_command(const char* mnemonic, int operand)
 {
     int opcode = -1;
-    if (strcmp(mnemonic, "NOP") == 0) opcode = CMD_NOP;
-    else if (strcmp(mnemonic, "CPUINFO") == 0) opcode = CMD_CPUINFO;
-    else if (strcmp(mnemonic, "READ") == 0) opcode = CMD_READ;
-    else if (strcmp(mnemonic, "WRITE") == 0) opcode = CMD_WRITE;
-    else if (strcmp(mnemonic, "LOAD") == 0) opcode = CMD_LOAD;
-    else if (strcmp(mnemonic, "STORE") == 0) opcode = CMD_STORE;
-    else if (strcmp(mnemonic, "ADD") == 0) opcode = CMD_ADD;
-    else if (strcmp(mnemonic, "SUB") == 0) opcode = CMD_SUB;
-    else if (strcmp(mnemonic, "DIVIDE") == 0) opcode = CMD_DIVIDE;
-    else if (strcmp(mnemonic, "MUL") == 0) opcode = CMD_MUL;
-    else if (strcmp(mnemonic, "AND") == 0) opcode = CMD_AND;
-    else if (strcmp(mnemonic, "MUL2") == 0) opcode = CMD_MUL2;
-    else if (strcmp(mnemonic, "DIV2") == 0) opcode = CMD_DIV2;
-    else if (strcmp(mnemonic, "JUMP") == 0) opcode = CMD_JUMP;
-    else if (strcmp(mnemonic, "JNEG") == 0) opcode = CMD_JNEG;
-    else if (strcmp(mnemonic, "HALT") == 0) opcode = CMD_HALT;
-    else if (strcmp(mnemonic, "NOT") == 0) opcode = CMD_NOT;
+    if (strcmp(mnemonic, "NOP") == 0)
+        opcode = CMD_NOP;
+    else if (strcmp(mnemonic, "CPUINFO") == 0)
+        opcode = CMD_CPUINFO;
+    else if (strcmp(mnemonic, "READ") == 0)
+        opcode = CMD_READ;
+    else if (strcmp(mnemonic, "WRITE") == 0)
+        opcode = CMD_WRITE;
+    else if (strcmp(mnemonic, "LOAD") == 0)
+        opcode = CMD_LOAD;
+    else if (strcmp(mnemonic, "STORE") == 0)
+        opcode = CMD_STORE;
+    else if (strcmp(mnemonic, "ADD") == 0)
+        opcode = CMD_ADD;
+    else if (strcmp(mnemonic, "SUB") == 0)
+        opcode = CMD_SUB;
+    else if (strcmp(mnemonic, "DIVIDE") == 0)
+        opcode = CMD_DIVIDE;
+    else if (strcmp(mnemonic, "MUL") == 0)
+        opcode = CMD_MUL;
+    else if (strcmp(mnemonic, "AND") == 0)
+        opcode = CMD_AND;
+    else if (strcmp(mnemonic, "JUMP") == 0)
+        opcode = CMD_JUMP;
+    else if (strcmp(mnemonic, "JNEG") == 0)
+        opcode = CMD_JNEG;
+    else if (strcmp(mnemonic, "HALT") == 0)
+        opcode = CMD_HALT;
+    else if (strcmp(mnemonic, "NOT") == 0)
+        opcode = CMD_NOT;
+    else if (strcmp(mnemonic, "JP") == 0)
+        opcode = CMD_JP;
+    else if (strcmp(mnemonic, "JNP") == 0)
+        opcode = CMD_JNP;
 
     if (opcode == -1) {
         fprintf(stderr, "Invalid mnemonic: %s\n", mnemonic);
@@ -67,12 +83,13 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    int memory[MEMORY_SIZE] = {0}; // Initialize all memory to 0
+    int memory[MEMORY_SIZE] = {0};
     char line[256];
 
     while (fgets(line, sizeof(line), in)) {
         char* comment_pos = strchr(line, ';');
-        if (comment_pos) *comment_pos = '\0';
+        if (comment_pos)
+            *comment_pos = '\0';
 
         char addr_str[10], token[10], operand_str[10];
         int addr;
@@ -89,11 +106,15 @@ int main(int argc, char* argv[])
                 if (sscanf(operand_str, "%d", &value) == 1) {
                     memory[addr] = value;
                 } else {
-                    fprintf(stderr, "Invalid value for address %d: %s\n", addr, operand_str);
+                    fprintf(stderr,
+                            "Invalid value for address %d: %s\n",
+                            addr,
+                            operand_str);
                 }
             } else {
                 int operand;
-                if (sscanf(operand_str, "%d", &operand) == 1 && operand >= 0 && operand <= 127) {
+                if (sscanf(operand_str, "%d", &operand) == 1 && operand >= 0
+                    && operand <= 127) {
                     int encoded = encode_command(token, operand);
                     if (encoded != -1) {
                         memory[addr] = encoded;
